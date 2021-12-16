4a289aa83c762fff93f2a94a81cff1605074700f
Julio Zaravia <julio.zaravia@chicho.pe>
2021-12-15 23:44:50 -0500

    UPDATE import/export file to support both types of component presentation

--- src/component-patterns/pages/ShoppingPage.tsx
+++ src/component-patterns/pages/ShoppingPage.tsx
@@ -23,11 +23,11 @@ export const ShoppingPage = () => {
           flexWrap: 'wrap',
         }}
       >
-        {/* <ProductCard product={product}>
+        <ProductCard product={product}>
           <ProductCard.Image />
           <ProductCard.Title title="Hola mundo!" />
           <ProductCard.Buttons />
-        </ProductCard> */}
+        </ProductCard>
         <ProductCard product={product}>
           <ProductImage />
           <ProductTitle title="Hola mundo!" />
