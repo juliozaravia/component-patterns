05be1e026ae4fdf58a700680521364e01e68b490
Julio Zaravia <julio.zaravia@chicho.pe>
2021-12-23 12:11:43 -0500

    UDPATE useProduct hook to limit counter

--- src/component-patterns/hooks/useProduct.ts
+++ src/component-patterns/hooks/useProduct.ts
@@ -16,21 +16,34 @@ export const useProduct = ({
 }: useProductArgs) => {
   const [counter, setCounter] = useState<number>(initialValues?.count || value);
 
+  const isMounted = useRef(false);
   const isControlled = useRef(!!onChange);
+
+  console.log('Initial values!');
 
   const increaseBy = (value: number) => {
     if (isControlled.current) {
       return onChange!({ count: value, product });
     }
 
-    const newValue = Math.max(counter + value, 0);
+    let newValue = Math.max(counter + value, 0);
+
+    if (initialValues?.maxCount) {
+      newValue = Math.min(newValue, initialValues.maxCount)
+    }
+
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
   };
 
   useEffect(() => {
+    if (!isMounted.current) return;
     setCounter(value);
   }, [value]);
+
+  useEffect(() => {
+    isMounted.current = true;
+  }, []);
 
   return {
     counter,
